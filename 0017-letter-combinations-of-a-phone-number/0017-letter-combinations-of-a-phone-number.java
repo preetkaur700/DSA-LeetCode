30a2c01b9c315fc1d387cf441b30804b4cd94e9d
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-02-17 11:43:44 +0530

    Time: 3 ms (33.34%), Space: 49.1 MB (19.46%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        leter(digits,0,result,"");
+        return result;
+    }
+
+    public void leter(String digits,int index,List<String>result,String ans){
+
+        String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        if(index==digits.length()){
+            result.add(ans);
+            return;
+        }
+
+        String s = map[digits.charAt(index)-'0'];
+
+        for(int i=0;i<s.length();i++){
+            leter(digits,index+1,result,ans+s.charAt(i));
+        }
+
+    }
+}
