7b5a0f73c3bf4665e922c3453b37df4d567a0675
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-02-26 10:38:54 +0530

    Time: 1 ms (88.03%), Space: 44.3 MB (38.14%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,23 +1,38 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        int n=nums.length;
+        int sets = 1<<n; //2^n
         List<List<Integer>> result = new ArrayList<>();
-        set(nums,0,result,new ArrayList<>());
+        for(int i=0;i<sets;i++){
+            List<Integer> ans = new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0){
+                    ans.add(nums[j]);
+                }
+            }
+        result.add(ans);
+        }
         return result;
     }
-    public void set(int[] nums,int i,List<List<Integer>>result,List<Integer>ans){
+        // List<List<Integer>> result = new ArrayList<>();
+        // set(nums,0,result,new ArrayList<>());
+        // return result;
+    // }
 
-        if(i==nums.length){
-            result.add(new ArrayList<>(ans));
-            return;
-        }
+    // public void set(int[] nums,int i,List<List<Integer>>result,List<Integer>ans){
 
-        // result.add(new ArrayList<>());
-        ans.add(nums[i]);
-        set(nums,i+1,result,ans);
+    //     if(i==nums.length){
+    //         result.add(new ArrayList<>(ans));
+    //         return;
+    //     }
 
-        ans.remove(ans.size()-1);
-        set(nums,i+1,result,ans);
+    //     // result.add(new ArrayList<>());
+    //     ans.add(nums[i]);
+    //     set(nums,i+1,result,ans);
+
+    //     ans.remove(ans.size()-1);
+    //     set(nums,i+1,result,ans);
         
 
-    }
+    // }
 }
