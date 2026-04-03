e3a99a5bf8d7718a678667364a5fb4a2639b478d
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-04-03 10:18:12 +0530

    Time: 1 ms (88.33%), Space: 44.4 MB (13.27%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,38 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        int sets = 1<<n; //2^n
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<sets;i++){
-            List<Integer> ans = new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i&(1<<j))!=0){
-                    ans.add(nums[j]);
-                }
-            }
-        result.add(ans);
-        }
+        List<List<Integer>>result=new ArrayList<>();
+        sub(nums,result,new ArrayList<>(),0);
         return result;
     }
-        // List<List<Integer>> result = new ArrayList<>();
-        // set(nums,0,result,new ArrayList<>());
-        // return result;
-    // }
-
-    // public void set(int[] nums,int i,List<List<Integer>>result,List<Integer>ans){
-
-    //     if(i==nums.length){
-    //         result.add(new ArrayList<>(ans));
-    //         return;
-    //     }
-
-    //     // result.add(new ArrayList<>());
-    //     ans.add(nums[i]);
-    //     set(nums,i+1,result,ans);
-
-    //     ans.remove(ans.size()-1);
-    //     set(nums,i+1,result,ans);
+    public void sub(int[] nums,List<List<Integer>>result,List<Integer>ans,int i)
+    {
+        if(i==nums.length){
+            result.add(new ArrayList<>(ans));
+            return;
+        }
         
+            ans.add(nums[i]);
+            sub(nums,result,ans,i+1);
 
-    // }
+            ans.remove(ans.size()-1);
+            sub(nums,result,ans,i+1);
+        
+        
+    }
 }
