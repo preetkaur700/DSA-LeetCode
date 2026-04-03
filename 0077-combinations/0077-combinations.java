963c11e7e0e851f83ee551907c802a05060f13db
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-04-03 11:50:06 +0530

    Time: 18 ms (83.04%), Space: 97.9 MB (87.03%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        comb(n,k,1,result,new ArrayList<>());
-        return result; 
+        
+        List<List<Integer>>result = new ArrayList<>();
+        comb(n,k,result,new ArrayList<>(),1);
+        return result;
     }
-    public void comb(int n,int k,int index,List<List<Integer>>result,ArrayList<Integer> ans){
-
+    public void comb(int n,int k,List<List<Integer>>result,List<Integer>ans,int i){
         if(ans.size()==k){
             result.add(new ArrayList<>(ans));
+            return;
         }
-
-        for(int i=index;i<=n;i++){
-            ans.add(i);
-            comb(n,k,i+1,result,ans);
-
+        for(int j=i;j<=n;j++){
+            ans.add(j);
+            comb(n,k,result,ans,j+1);
             ans.remove(ans.size()-1);
-        }
-
+        }     
     }
 }
