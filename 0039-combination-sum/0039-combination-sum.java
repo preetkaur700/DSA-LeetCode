ebf9a401993db5f76a55a861a631a8da01ac15aa
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-04-03 11:58:35 +0530

    Time: 2 ms (95.58%), Space: 46 MB (25.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> result = new ArrayList<>();
-       sum(candidates,target,0,result,new ArrayList<>());
+       List<List<Integer>>result=new ArrayList<>();
+       comb(candidates,target,result,new ArrayList<>(),0);
        return result;
     }
+    public void comb(int[] nums,int t,List<List<Integer>>result,List<Integer>ans,int i){
     
-    public void sum(int[] cd,int target,int i,List<List<Integer>> result, List<Integer> ans){
-        if(i==cd.length){
-            if(target==0){
-                result.add(new ArrayList<>(ans));
-            }
-            return;
+    if(i==nums.length){
+        if(t==0){
+            result.add(new ArrayList<>(ans));
         }
-
-        if(cd[i]<=target){
-            ans.add(cd[i]);
-            sum(cd,target-cd[i],i,result,ans);
+        return;
+    }
+        if(nums[i]<=t){
+            ans.add(nums[i]);
+            comb(nums,t-nums[i],result,ans,i);
             ans.remove(ans.size()-1);
         }
-        sum(cd,target,i+1,result,ans);
-        
+        comb(nums,t,result,ans,i+1);
+
     }
+
 
 }
