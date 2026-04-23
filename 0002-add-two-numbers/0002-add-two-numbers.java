ee17e4bc54674b165627d933350c2ec3800f3754
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-04-23 10:13:17 +0530

    Time: 1 ms (100%), Space: 46.6 MB (22.4%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,29 +9,44 @@
  * }
  */
 class Solution {
-    
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-          ListNode a=l1;
-          ListNode b=l2;
-          ListNode dummy = new ListNode(0);
-          ListNode curr = dummy ;
-          int carry=0;
-          while(a!=null || b!=null || carry != 0){
-               
-                int sum=carry;
-                if(a!=null){
-                  sum+=a.val;
-                  a=a.next;
-                }
-                if(b!=null){
-                    sum+=b.val;
-                    b=b.next;
-                }
-                carry = sum/10;
-                int digit = sum%10;
-                curr.next = new ListNode(digit); 
-                curr=curr.next;
-          }
-          return dummy.next;
+        
+        // ListNode temp1 = reverse(l1);
+        // ListNode temp2 = reverse(l2);
+        ListNode dummy = new ListNode(0);
+        ListNode a = l1;
+        ListNode b = l2;
+        ListNode temp = dummy;
+        int carry = 0;
+        while(a!=null || b!=null || carry!=0){
+            int sum=carry;
+            if(a!=null){
+                sum+=a.val;
+                a=a.next;
+            }
+            if(b!=null){
+                sum+=b.val;
+                b=b.next;
+            }
+
+            carry = sum/10;
+            int digit = sum%10;
+            temp.next = new ListNode(digit);
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+    public ListNode reverse(ListNode head){
+
+        ListNode temp=head;
+        ListNode prev=null;
+        ListNode next=null;
+        while(temp!=null){
+            next=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=next;
+        }
+        return head;
     }
 }
