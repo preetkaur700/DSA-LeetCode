012634c1d7678c8ef43d4d13532868ee4ff3aa2c
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-07-03 14:35:15 +0530

    Time: 32 ms (78.98%), Space: 59.4 MB (32.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,76 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-/////////optimal using two pointer////
-    List<List<Integer>> result = new ArrayList<>();
-    int n = nums.length;
-    Arrays.sort(nums);
-    
-    for(int i=0;i<n-2;i++){
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
 
-        if(i>0 && nums[i]==nums[i-1])continue;
-        int j=i+1, k=n-1;
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
 
-        while(j<k){
-            int sum = nums[i]+nums[j]+nums[k];
-           
-            if(sum<0){
-                j++;
-            }else if(sum>0){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
 
-                k--;          
-            }else{
-                result.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                j++;
-                k--;
-            
-
-                while(j<k && nums[j]==nums[j-1])j++;
-
-
-            } 
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1])left++;
+                    while(left<right  && nums[right]==nums[right-1])right--;
+                    left++;
+                    right--;
+                }else if(sum>0){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
         }
-    }
-    return result;
-        
-/////////////better optimal using hashing method with lop////////////
-            // HashSet<List<Integer>> result = new HashSet<>();
-            // int n = nums.length;
-            // for(int i=0;i<n;i++){
-            //     HashSet<Integer>set= new HashSet<>();
-            //     for(int j=i+1;j<n;j++){
-            //         int target = -(nums[i]+nums[j]);
-            //         c
-            //         if(set.contains(target)){
-            //             List<Integer>st = Arrays.asList(nums[i],nums[j],target);
-            //             Collections.sort(st);
-            //             result.add(st);
-            //         }
-            //         set.add(nums[j]);
-            //     }                
-            // }
-            // return new ArrayList<>(result);
-           ///////////////brute force////////////
+        return result;
 
-            // HashSet<List<Integer>> set = new HashSet<>();
-            // for(int i=0;i<n;i++){
-            //     for(int j=i+1;j<n;j++){
-            //         for(int k=j+1;k<n;k++){
-            //             if(nums[i]+nums[j]+nums[k]==0){
-            //                 List<Integer> ans = Arrays.asList(nums[i],nums[j],nums[k]);
-            //                 Collections.sort(ans);
-
-            //                 if(!set.contains(ans)){
-            //                     set.add(ans);
-            //                     result.add(ans);
-                                                               
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }            
-            // return result;
-
-    
-                      
     }
 }
