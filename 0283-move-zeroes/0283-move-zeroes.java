1325d42f5bebcca467fea457e79ca212d675801f
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-07-06 10:17:16 +0530

    Time: 2 ms (92.13%), Space: 47.4 MB (93.31%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        int right=0;
+        while(left<nums.length){
+            if(nums[left]!=0){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                right++;
+            }
+            left++;
+
+        }
+    }
+}
