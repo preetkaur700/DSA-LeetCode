562b18a81820cfcf1439505c1f19d42cf84c17b2
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-07-07 20:21:22 +0530

    Time: 5 ms (82.38%), Space: 77.7 MB (12.34%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,18 +3,16 @@ class Solution {
         int n=height.length;
         int i=0;
         int j=n-1;
-        int max=0;
+        int area=0;
         while(i<j){
-            int width = j-i;
-            int minheight = Math.min(height[i],height[j]);
-            int area = width*minheight;
-            max = Math.max(max,area);
-            if(height[i]<height[j]){
-                i++;
-            }else{
-                j--;
-            }
-        }        
-        return max;
+            int h=Math.min(height[i],height[j]);
+            int w=j-i;
+            area=Math.max(area,h*w);
+
+            if(height[i]<height[j])i++;
+            else j--;   
+
+        }
+        return area;
     }
 }
