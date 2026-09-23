966ee9d11774fe22e6f28344f7843484be46b643
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2026-09-23 20:45:49 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.84%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,20 +1,20 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-       //find pivot
-        int pivot=-1;
+        int n = nums.length;
+        int pivot = -1;
+
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                pivot = i;
+                pivot=i;
                 break;
             }
         }
-
         if(pivot==-1){
-                int i=0;
-                int j=n-1;
+            int i=0;
+            int j=n-1;
+
             while(i<=j){
-                int temp=nums[i];
+                int temp = nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
                 i++;
@@ -22,29 +22,24 @@ class Solution {
             }
             return;
         }
-
-        //find rightmost digit which is greater than pivot
         for(int i=n-1;i>pivot;i--){
             if(nums[i]>nums[pivot]){
-                swap(nums,pivot,i);
+                swap(nums,i,pivot);
                 break;
             }
         }
-        
-        //reverse
         int i=pivot+1;
-        int j=n-1;
+        int j = n-1;
         while(i<=j){
-            int temp=nums[i];
+            int temp = nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
             i++;
             j--;
         }
-       
     }
     public void swap(int[] nums,int i,int j){
-        int temp=nums[i];
+        int temp = nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
     }
